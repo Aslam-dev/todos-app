2a8809e6a0efa02d3ea86f336e7b8c50efdf11ef
Aslam-dev <almaslam01@gmail.com>
2024-09-24 13:51:07 +0530

    next js simple front end added

--- todo-be/index.js
+++ todo-be/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.use("", require("./routes/todos"));
+app.use("/todos", require("./routes/todos"));
 
 app.listen(5000, () =>{
     console.log("now listening port 5000");
